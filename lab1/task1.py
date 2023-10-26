36929fbaff61206ed7e2a730fb87e11be6af8beb
Карпенко Глеб <glebkarpenko1@gmail.com>
2023-10-26 13:55:46 +0300

    optimized

--- lab1/task1.py
+++ lab1/task1.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 df = pd.read_excel('task.xlsx', sheet_name='task1')
-result_df = df.iloc[:,] * df.iloc[-1]
+result_df = df * df.iloc[-1]
 result_df = result_df.iloc[:-1]
-row_sums = result_df.sum(axis=1)
+sum_column = result_df.sum(axis=1)
 
 def print_results():
     print('Оцінки критеріїв для альтернатив та їх вага: ')
     print(df)
     print('Значення привабливості альтернатив на основі оцінок їх критеріїв та ваги: ')
     print(result_df)
-    print(row_sums)
+    print(sum_column)
     print("Альтернатива з максимальним значенням привабливості: ")
-    print('A{}'.format(row_sums.idxmax() + 1))
+    print('A{}'.format(sum_column.idxmax() + 1))
 
 print_results()
